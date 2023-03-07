a43c4bdce606d24af08801741f5bed38712ec2c4
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-03-07 10:38:28 -0500

    removed leftovers from previous project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,9 @@ const Header = props => {
     return(
         <header className="fixed top-0 left-0 right-0 flex justify-between bg-gray-200 w-full">
             <Link to="/">
-                <div>Shopathon</div>
+                <div>Django-React-Template</div>
             </Link>
             <div className="flex">
-                <button className="bg-purple-500">Cart</button>
                 <LogoutButton />
             </div>
         </header>
